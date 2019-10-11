3df7ef411b6655fab30a1f2e2d4e233aef35edf0
VuTranQuang <vutran7993@gmail.com>
2019-10-11 10:09:38 +0700

    Initial Commit

---
+++ SimpleUIWebView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SimpleUIWebView
+//
+//  Created by RTC-HN154 on 10/11/19.
+//  Copyright © 2019 RTC-HN154. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
